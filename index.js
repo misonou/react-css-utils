3f35cd9e76a16ac5557cb30fb60662185a5795e1
Takeru <takeru.moe@gmail.com>
2024-09-11 18:53:39 +0800

    fix: handle unsortable elm in the same container

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import $ from "jquery";
 import dom, { reportError } from "zeta-dom/dom";
-import { bind, containsOrEquals, getContentRect, getRect, isVisible, removeNode, scrollIntoView, setClass, toPlainRect } from "zeta-dom/domUtil";
+import { bind, containsOrEquals, getContentRect, getRect, isVisible, matchSelector, removeNode, scrollIntoView, setClass, toPlainRect } from "zeta-dom/domUtil";
 import { always, createPrivateStore, definePrototype, each, either, extend, is, isPlainObject, makeArray, makeAsync, mapRemove, matchWord, matchWordMulti, setImmediate, setTimeout } from "zeta-dom/util";
 import { createAutoCleanupMap } from "zeta-dom/observe";
 import { useDispose } from "zeta-dom-react";
@@ -323,7 +323,10 @@ export function initSortable(element, options) {
             return;
         }
         const container = item.parentElement;
-        const children = makeArray(container.children);
+        const children = makeArray(container.children).filter(function (v) {
+            return matchSelector(v, options.itemSelector);
+        });
+        const lastBefore = children.slice(-1)[0].nextElementSibling;
         const curIndex = children.indexOf(item);
         const scrollable = e.target.closest('[scrollable-target]');
         const r0 = getRect(scrollable);
@@ -351,9 +354,9 @@ export function initSortable(element, options) {
         setClass(item, 'dragging', true);
         always(promise, function (result) {
             if (result && newIndex !== curIndex) {
-                container.insertBefore(item, children[newIndex + +(newIndex > curIndex)]);
+                container.insertBefore(item, children[newIndex + +(newIndex > curIndex)] || lastBefore);
                 makeAsync(options.onOrderChanged)(curIndex, newIndex).catch(function () {
-                    container.insertBefore(item, children[curIndex + 1]);
+                    container.insertBefore(item, children[curIndex + 1] || lastBefore);
                     reportError(e, container);
                 });
             }
